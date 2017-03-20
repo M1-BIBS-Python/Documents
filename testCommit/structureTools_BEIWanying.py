6370b3eaa62e0e31d67adab28f4399b3c8f5ad26
Wanying Bei <wbei@a-018843.ups.u-psud.fr>
2017-03-20 15:51:36 +0100

    test d'une modification de fichier

--- testCommit/structureTools_BEIWanying.py
+++ testCommit/structureTools_BEIWanying.py
@@ -2,6 +2,48 @@
 # -*- coding: utf-8 -*-
 import math
 
+
+
+def dico(lines):
+	dico_prot = dict()
+
+	for line in lines:
+		if line[0:4]=="ATOM":
+			chain = line[21]
+	
+			if chain not in dico_prot.keys():
+				dico_prot[chain] = dict()
+				dico_prot[chain]["reslist"]=[]
+	
+			res = line[22:26]
+			if res not in dico_prot[chain].keys():
+				dico_prot[chain][res] = dict()
+				dico_prot[chain]["reslist"].append(res)
+		
+			residu = line[17:20]
+			if residu not in dico_prot[chain][res].keys():
+				dico_prot[chain][res][residu] = dict()
+				dico_prot[chain][res][residu]["atomlist"]=[]
+	
+			atom = line[13:16]
+			if atom not in dico_prot[chain][res][residu].keys():
+				dico_prot[chain][res][residu][atom] = dict()
+				dico_prot[chain][res][residu]["atomlist"].append(atom)
+		
+			dico_prot[chain][res][residu][atom]["x"]=line[31:38]
+			dico_prot[chain][res][residu][atom]["y"]=line[39:46]
+			dico_prot[chain][res][residu][atom]["z"]=line[47:54]
+
+	return dico_prot
+
+	
+	
+	
+	
+
+
+
+
 def CDM(dico1,dico2):
 	dico_mass=dict()
 	dico_mass['C']=12
