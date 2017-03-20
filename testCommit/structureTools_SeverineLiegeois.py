b494606847d026a8ca6cbf2e1c90479a7eaa4ef2
Severine Liegeois <sliegeo@a-018835.ups.u-psud.fr>
2017-03-20 17:00:51 +0100

    test

--- testCommit/structureTools_SeverineLiegeois.py
+++ testCommit/structureTools_SeverineLiegeois.py
@@ -12,6 +12,8 @@ Description:
 from math import sqrt
 import string
 import re
+import matplotlib.pyplot as plt
+import numpy as np
 
 def ParsingPDB (pdbFile):
 		
@@ -28,10 +30,10 @@ def ParsingPDB (pdbFile):
 		if line[:4:] == 'ATOM':						# si la ligne commence par 'ATOM'
 			
 			if cptAlt == False:
-				alt = line[17]
+				alt = line[16]
 				cptAlt = True
 				
-			if line[17] == alt:
+			if line[16] == alt:
 				chaine = line[21]
 				
 				if chaine not in chainList:
@@ -69,47 +71,91 @@ def ParsingPDB (pdbFile):
 	return(molecule)
 	
 
-def ContactResidus(dico):
+def CentreMasse(dico):
 	
 	atomicMass = dict()
 	atomicMass = {"C": float(12.0107), "N": float(14.0067), 
 				  "O": float(15.9994), "S": float(32.065),
 				  "OH": float(17.0073), "NH": float(15.0146)}
 	
-	# calcul du centre de masse:
 	x = 0
 	y = 0
 	z = 0
 	massTot = 0
-	CM = {}
+	CM_res = {}
+	for atom in dico.keys():
+		if atom != 'resname' and atom != 'atomlist':
+			for key in atomicMass.keys():
+				if re.match(key, atom):
+					mass = atomicMass[key]
+			x += mass*dico[atom]['x']
+			y += mass*dico[atom]['y']
+			z += mass*dico[atom]['z']
+			massTot += mass
+	# coordonnees du centre de masse du residu:
+	CM_res['x'] = x/massTot
+	CM_res['y'] = y/massTot
+	CM_res['z'] = z/massTot
+
+	return CM_res
+
+def CentreGravite(dico):
+	x = 0
+	y = 0
+	z = 0
+	nbAtomes = 0
+	CG_res = {}
+	for atom in dico.keys():
+		if atom != 'resname' and atom != 'atomlist':
+			x += dico[atom]['x']
+			y += dico[atom]['y']
+			z += dico[atom]['z']
+			nbAtomes += 1
+	# coordonnees du centre de gravite du residu:
+	CG_res['x'] = x/nbAtomes
+	CG_res['y'] = y/nbAtomes
+	CG_res['z'] = z/nbAtomes
 	
+	return CG_res
+
+# distance entre 2 points dans l'espace:
+def Distance(res1, res2):
+	return sqrt((res1['x']-res2['x'])*(res1['x']-res2['x'])+(res1['y']-res2['y'])*(res1['y']-res2['y'])+(res1['z']-res2['z'])*(res1['z']-res2['z']))
+
+# retourne la matrice des distances 2 a 2 des residus:
+def MatriceDistances(dico):
+	seuil = 4
 	for chain in dico.keys():
-		for res in dico[chain].keys():
-			if res != 'reslist':
-				for atom in dico[chain][res].keys():
-					if atom != 'resname' and atom != 'atomlist':
-						for key in atomicMass.keys():
-							if re.match(key, atom):
-								mass = atomicMass[key]
-						x += mass*dico[chain][res][atom]['x']
-						y += mass*dico[chain][res][atom]['y']
-						z += mass*dico[chain][res][atom]['z']
-						massTot += mass
-
-	CM['x'] = x/massTot
-	CM['y'] = y/massTot
-	CM['z'] = z/massTot
-	
-	return CM
-
-
-			
-
+		distMatrix = []
+		enContact = {}
+		for res1 in dico[chain].keys():
+			if res1 != 'reslist':
+				r1 = CentreGravite(dico[chain][res1])
+				listr1 = [] # liste qui contiendra les distances entre r1 et tous les autres residus
+				for res2 in dico[chain].keys():
+					if res2 != 'reslist':
+						r2 = CentreGravite(dico[chain][res2])
+						listr1.append(Distance(r1, r2))
+						if (Distance(r1,r2) <= seuil and Distance(r1,r2) != 0 and (res2, res1) not in enContact.keys()):
+							enContact[res1,res2] = Distance(r1, r2)
+				distMatrix.append(listr1)
+				
+		
+		data = np.array(distMatrix).reshape(len(listr1), len(listr1))
+		heatmap = plt.pcolor(data, cmap="RdBu_r")
+		
+		
+		#plt.show() # montre la heatmap dans une fenetre
+		plt.savefig("/home/tp-home007/sliegeo/Documents/Python/heatmap.png")
+		return enContact
+		
 
 if __name__ == '__main__':
-	dico = ParsingPDB("../Data/arginine.pdb")
-	print ContactResidus(dico)
-	
+	dico = ParsingPDB("/home/tp-home007/sliegeo/Documents/Python/2w18.pdb")
+	#print dico
+	#print "Centre de masse : " + str(CentreMasse(dico))
+	#print "Centre de gravite : "+ str(CentreGravite(dico))
+	print MatriceDistances(dico)
 
 	
 			
